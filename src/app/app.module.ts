2b5f90788af9cb498e7e8be2891b377ad5d95d24
scott <scotty.wright30gmail.com>
2019-04-21 21:53:38 +0100

    Added initial auth services and created the page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { Camera } from '@ionic-native/camera/ngx';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    {
+      provide: RouteReuseStrategy, useClass: IonicRouteStrategy,
+
+    },
+    { provide: FirestoreSettingsToken, useValue: {} },
+    // Camera,
+    Camera,
+    Firebase
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/pages/start-chat/start-chat.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-chat',
+  templateUrl: './start-chat.page.html',
+  styleUrls: ['./start-chat.page.scss'],
+})
+export class StartChatPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
